9a736c47d09ad4c1025d716794aed806ddf9ad5d
Adin Karic <akaric@student.tgm.ac.at>
2014-05-08 16:53:11 +0200

    Erster Commit, Schritt 1

---
+++ src/anilkaric/SubstitutionCipher.java
@@ -0,0 +1,28 @@
+package anilkaric;
+
+/**
+ * Klasse die von MonoAlphabeticCipher erbt
+ * @author Adin Karic
+ * @version 2014-04-08
+ *
+ */
+public class SubstitutionCipher extends MonoAlphabeticCipher {
+	/**
+	 * Konstruktor dem ein geheimalphabet übergeben wird
+	 * @param secretAlphabet das geheimalphabet als string
+	 * @throws MyException
+	 */
+	public SubstitutionCipher(String secretAlphabet) throws MyException{
+		//Methode von Superklasse wird aufgerufen
+		super.setSecretAlphabet(secretAlphabet);
+	}
+	/**
+	 * Überschriebene Methode der Superklasse zum Setzen eines geheimalpahbets
+	 */
+	@Override
+	public void setSecretAlphabet(String secretAlphabet) throws MyException{
+		//Methode von Superklasse wird aufgerufen
+		super.setSecretAlphabet(secretAlphabet);
+	}
+	
+}
